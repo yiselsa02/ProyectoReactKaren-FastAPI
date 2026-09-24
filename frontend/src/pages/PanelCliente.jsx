c4ab12a8c784aa9c4919477b5811885ce22533f5
yiselsa02 <yisel.sa02@gmail.com>
2026-09-23 22:30:19 -0500

    djvfhhdfFDFDSJHvFbbvvcgfnG

--- frontend/src/pages/PanelCliente.jsx
+++ frontend/src/pages/PanelCliente.jsx
@@ -1113,7 +1113,7 @@ export default function PanelCliente({
                       </button>
                     </div>
                   )}
-                  </>}
+                  </>)}
               </section>
             )}
 
